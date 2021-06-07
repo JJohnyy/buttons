25ad48c306d852257cfa46094ab4367b01f41602
JJohnyy <84328224+JJohnyy@users.noreply.github.com>
2021-06-07 19:57:43 +0200

    added js

---
+++ scripts.js
@@ -0,0 +1,3 @@
+$('#one').on ('click', () => {console.log('Yeah, you clicked me')});
+$('#two').on ('click', () => {$ ('#one').text('TaDA!')});
+$('#three').on ('click', () => {$ ('button').css('background-color', 'green')});
