8bbd4d3f97caa8cc2271364fb06825ed2db99c09
JJohnyy <84328224+JJohnyy@users.noreply.github.com>
2021-06-08 07:35:17 +0200

    added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-$('#one').on ('click', () => {console.log('Yeah, you clicked me')});
-$('#two').on ('click', () => {$ ('#one').text('TaDA!')});
-$('#three').on ('click', () => {$ ('button').css('background-color', 'green')});
+$('#1').on ('click', () => {console.log('Yeah, you clicked me')})
+$('#2').on ('click', () => { $('#1').text('TaDA!')})
+$('#3').on ('click', () => { $('button').css('background-color', 'green')})
+
+function changeColor() 
+ {document.getElementById.trigger("#Myelement", "#1", "#2").style.backgroundColor = 
+ document.getElementById("MyColorPicker").value;}
+
+ 
+ 
+
+
+ 
